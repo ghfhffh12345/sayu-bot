d86b75284e358cb8edf2922deeba24be121ad57b
ghfhffh12345 <ghfhffh12345@outlook.kr>
2021-09-26 18:46:16 +0900

    더욱 강력한 방어

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,6 +1,10 @@
+const pp = ['프로페타', 'p.p', 'pp', 'profeta']
+
 module.exports = {
     name: "guildMemberAdd",
     execute(member, client) {
-        if (member.id == '865390121075605504') member.kick()
+        if (member.id == '865390121075605504' || pp.indexOf(member.user.username) != -1) {
+            member.kick()
+        }
     }
 }
