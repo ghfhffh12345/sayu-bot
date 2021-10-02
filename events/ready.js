066f179d9eb4518848a7ac57187a60bac7ef5bcf
ghfhffh12345 <ghfhffh12345@outlook.kr>
2021-10-02 10:37:52 +0900

    너의 이름은 조아

--- events/ready.js
+++ events/ready.js
@@ -5,6 +5,6 @@ module.exports = {
     once: true,
     execute(client) {
         console.log('Ready!')
-        client.user.setActivity(`${prefix}help || ${client.guilds.cache.size}개의 서버에서 활동 중!`)
+        client.user.setActivity(`${prefix}help | ${client.guilds.cache.size}개의 서버에서 활동 중! | '너의 이름은' 보는 중...`)
     }
 }
